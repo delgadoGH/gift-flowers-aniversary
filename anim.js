1589c4024e1291959b2375cfbdea8b0cd3505175
delgadoGH <delgadohabraam@gmail.com>
2026-09-22 09:00:46 -0400

    Update anim.js

--- anim.js
+++ anim.js
@@ -11,10 +11,10 @@ var lyricsData = [
   { text: "Tell me baby, tell me, tell me baby", time: 173 },
   { text: "what you trying to do?", time: 175 },
   { text: "Cool jewelry Shining bright.", time: 177 },
-  { text: "Strawberry champagne for you.", time: 178 },
-  { text: "That's what I like. That's what I like.", time: 180 },
-  { text: "That's what I like by the fire and diamonds.", time: 184 },
-  { text: "I like it. That's why I like it.", time: 188 }
+  { text: "Strawberry champagne for you.", time: 179 },
+  { text: "That's what I like. That's what I like.", time: 182 },
+  { text: "That's what I like by the fire and diamonds.", time: 186 },
+  { text: "I like it. That's why I like it.", time: 190 }
 ];
 
 // Animar las letras
