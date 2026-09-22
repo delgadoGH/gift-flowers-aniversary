49200dab101deba30ebed140797a54a13c843e4d
delgadoGH <delgadohabraam@gmail.com>
2026-09-22 08:52:46 -0400

    Update anim.js

--- anim.js
+++ anim.js
@@ -8,9 +8,9 @@ var lyricsData = [
   { text: "What's on your mind?", time: 162 },
   { text: "If you want it, girl, come and get it.", time: 166 },
   { text: "This is here for you.", time: 169 },
-  { text: "Tell me baby, tell me, tell me baby, what you trying to do?", time: 173 },
-  { text: "Shining bright. Strawberry champagne for you.", time: 177 },
-  { text: "That's what I like. That's what I like.", time: 181 },
+  { text: "Tell me baby, tell me, tell me baby, what you trying to do?", time: 174 },
+  { text: "Shining bright. Strawberry champagne for you.", time: 178 },
+  { text: "That's what I like. That's what I like.", time: 182 },
   { text: "That's what I like by the fire and diamonds.", time: 186 },
   { text: "I like it. That's why I like it.", time: 192 }
 ];
